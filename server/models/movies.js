bcf339e31c0f7cdfcbd0faedff20cac3f7629ed3
Jess <imjessw@gmail.com>
2015-08-08 21:56:20 -0400

    added a controllers folder. added movies file to my models folder

--- server/models/movies.js
+++ server/models/movies.js
@@ -3,5 +3,16 @@ var mongoose = require('mongoose');
 
 //Create MovieSchema.
 var MovieSchema = new mongoose.Schema({
-  
-})
+  title:{
+    type: String,
+    required: true
+  },
+  url:{
+    type: String,
+    required: true
+  }
+});
+
+
+// export model Schema
+module.exports = MovieSchema;
